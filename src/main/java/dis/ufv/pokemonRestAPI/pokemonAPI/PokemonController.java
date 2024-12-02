9e9db90e6f82ebf7f91854d54e98294fc1e151f3
RodrigoIbanez <9106387@alumnos.ufv.es>
2024-12-02 20:19:53 +0100

    Filtro pot tipo

--- src/main/java/dis/ufv/pokemonRestAPI/pokemonAPI/PokemonController.java
+++ src/main/java/dis/ufv/pokemonRestAPI/pokemonAPI/PokemonController.java
@@ -28,4 +28,16 @@ public class PokemonController {
         }
         return new ResponseEntity<>(encontrado, HttpStatus.OK);
     }
+
+    @GetMapping("/pokemon/porTipo/{tipo}")
+    public ArrayList<Pokemon>  getPortipo(@PathVariable String tipo){
+        ArrayList<Pokemon> listaPokemons = new LectorJson().leeFicheroJson("./src/main/resources/pokemonConId.json");
+        ArrayList<Pokemon> listaEncontrados = new ArrayList<>();
+        for (Pokemon pokemon : listaPokemons) {
+            if (pokemon.getTipo1().equalsIgnoreCase(tipo) || pokemon.getTipo2().equalsIgnoreCase(tipo)) {
+                listaEncontrados.add(pokemon);
+            }
+        }
+        return listaEncontrados;
+    }
 }
