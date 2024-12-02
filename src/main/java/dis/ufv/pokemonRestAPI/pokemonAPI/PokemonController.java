017bdfa5cece06dbe4c9d6eca00cee0587eacb96
RodrigoIbanez <9106387@alumnos.ufv.es>
2024-12-02 20:12:41 +0100

    busqueda por nombre

--- src/main/java/dis/ufv/pokemonRestAPI/pokemonAPI/PokemonController.java
+++ src/main/java/dis/ufv/pokemonRestAPI/pokemonAPI/PokemonController.java
@@ -1,6 +1,9 @@
 package dis.ufv.pokemonRestAPI.pokemonAPI;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -11,5 +14,17 @@ public class PokemonController {
     public ArrayList<Pokemon> pokemons(){
         ArrayList<Pokemon> listaPokemons = new LectorJson().leeFicheroJson("./src/main/resources/pokemonConId.json");
         return listaPokemons;
+    }
+
+    @GetMapping("/pokemon/porNombre/{nombre}")
+    public ResponseEntity<Pokemon> getPorNombre(@PathVariable String nombre){
+        ArrayList<Pokemon> listaPokemons = new LectorJson().leeFicheroJson("./src/main/resources/pokemonConId.json");
+        Pokemon encontrado = null;
+        for (Pokemon pokemon : listaPokemons) {
+            if (pokemon.getName().equalsIgnoreCase(nombre)) {
+                encontrado = pokemon;
+            }
+        }
+        return new ResponseEntity<>(encontrado, HttpStatus.OK);
     }
 }
